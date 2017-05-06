3cb691b7f099d8e314306f2860578599526e4937
makenneth <kenneth.km.ma@gmail.com>
2017-05-05 17:35:01 -0700

    Added auth component and social buttons

--- frontend/app/components/Application/index.js
+++ frontend/app/components/Application/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Main from 'components/Main';
+import Auth from 'components/Auth';
 import 'assets/reset.css';
 import './styles.scss';
 
@@ -7,6 +8,7 @@ class Application extends React.PureComponent {
   render() {
     return (
       <div className="app-container">
+        <Auth />
         <h3>letspaint.it</h3>
         <Main />
       </div>
