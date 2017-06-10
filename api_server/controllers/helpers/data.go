b9f7db4e423c35cd7c13d59866bd3dcfbe60c5a3
makenneth <kenneth.km.ma@gmail.com>
2017-06-10 14:31:02 -0700

    Fixed bug in formatData

---
+++ api_server/controllers/helpers/data.go
@@ -0,0 +1,33 @@
+package helpers
+
+import (
+  "encoding/json"
+  "log"
+  "reflect"
+  "strings"
+)
+
+type Data struct {
+  JsonData map[string]interface{} `json:"data"`
+}
+
+func lowercase(s string) string {
+  return strings.ToLower(s[:1]) + s[1:]
+}
+
+func getType(d interface{}) string {
+  if t := reflect.TypeOf(d); t.Kind() == reflect.Ptr {
+    return t.Elem().Name()
+  } else {
+    return t.Name()
+  }
+}
+
+func FormatData(d interface{}) json.RawMessage {
+  t := lowercase(getType(d))
+  jsonData := map[string]interface{}{
+    t: d,
+  }
+  data, _ := json.Marshal(&Data{jsonData})
+  return data
+}
